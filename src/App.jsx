562b726f448c035df9401fdbfd811e4c24528962
Bano <truong.supercode@gmail.com>
2023-08-16 18:17:08 +0200

    connection with github

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "dashboard",
+        element: <DashboardLayout />,
+      },
+    ],
+  },
+]);
+
+createBrowserRouter;
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
